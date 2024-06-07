3bd208f0f33c901a140c5eae65125d0671e8793e
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-06-07 11:35:38 -0300

    :sparkles: feat: Logica do todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styled.jsx";
 import apagar from "./assets/apagar.png";
 
 export default function App() {
+  // Captura Tarefa
+  const [captura, setCaptura] = useState("");
+
+  // Guarda tarefa
+  const [tarefas, setTarefas] = useState([
+    {
+      nome: "Tarefa 1",
+      id: 1,
+    },
+  ]);
+
+  // Adiciona tarefa
+  const addTarefa = () => {
+    // Criamos uma nova tarefa para concatenar no array []
+    const novaTarefa = {
+      nome: captura,
+      id: Math.random(),
+    };
+
+    // Verifica input vazio
+    if (captura === "") {
+      alert("Adicione uma tarefa");
+    } else {
+      setTarefas(tarefas.concat(novaTarefa));
+      setCaptura("");
+    }
+  };
+
+  // Remover tarefa
+  const remove = (id) => {
+    const tarefaRemovida = tarefas.filter((item) => item.id !== id);
+    setTarefas(tarefaRemovida);
+  };
+
   return (
     <>
       <S.Main>
         <S.Container>
           <h1>TODO LIST</h1>
+
           <S.Campo>
-            <input placeholder="Adicionar tarefa..." type="text" />
-            <button>+</button>
+            <input
+              onChange={(e) => setCaptura(e.target.value)}
+              placeholder="Adicionar tarefa..."
+              maxLength={20}
+              value={captura}
+              type="text"
+            />
+            <button onClick={() => addTarefa()}>+</button>
           </S.Campo>
-          <S.Lista>
-            <input type="checkbox" />
-            <h3>Tarefa 1</h3>
-            <button>
-              <img src={apagar} alt="apagar" />
-            </button>
-          </S.Lista>
+
+          <S.Box>
+            {tarefas.map((item, id) => (
+              <S.Lista key={id}>
+                <input type="checkbox" />
+                <h3>{item.nome}</h3>
+                <button onClick={() => remove(item.id)}>
+                  <img src={apagar} alt="apagar" />
+                </button>
+              </S.Lista>
+            ))}
+          </S.Box>
         </S.Container>
       </S.Main>
     </>
